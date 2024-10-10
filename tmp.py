e045bc58cb47ac0d5a3a58224477eaac7fbbb3a2
Lee Jun Hyuk <jmjh98@snu.ac.kr>
2024-10-11 01:32:24 +0900

    Initial Commit

---
+++ tmp.py
@@ -0,0 +1,34 @@
+import pandas as pd
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
+df.head()
+
+
+from sklearn.preprocessing import StandardScaler  # 표준화 패키지 라이브러리 
+x = df.drop(['target'], axis=1).values # 독립변인들의 value값만 추출
+y = df['target'].values # 종속변인 추출
+
+x = StandardScaler().fit_transform(x) # x객체에 x를 표준화한 데이터를 저장
+
+features = ['sepal length', 'sepal width', 'petal length', 'petal width']
+pd.DataFrame(x, columns=features).head()
+
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2) # 주성분을 몇개로 할지 결정
+printcipalComponents = pca.fit_transform(x)
+principalDf = pd.DataFrame(data=printcipalComponents, columns = ['principal component1', 'principal component2'])
+# 주성분으로 이루어진 데이터 프레임 구성
+
+
+principalDf.head()
+pca.explained_variance_ratio_
+
+pca = PCA(n_components=3)
+
+printcipalComponents = pca.fit_transform(x)
+
+principalDf = pd.DataFrame(data=printcipalComponents, columns = ['principal component1', 'principal component2', '3'])
+
+pca.explained_variance_ratio_
+
